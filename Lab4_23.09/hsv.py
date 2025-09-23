92db87cd7564884523aacd8a26fa5a0f0bbae34c
rhr18818 <rhr14101@gmail.com>
2025-09-23 10:33:46 +0600

    image processing---4th lab complete

--- Lab4_23.09/hsv.py
+++ Lab4_23.09/hsv.py
@@ -17,18 +17,6 @@ weighted_kernel = np.array([[1, 2, 1],
                             [1, 2, 1]], dtype=np.float32)
 weighted_kernel /= np.sum(weighted_kernel)  # normalize
 
-# test floor division
-# print("-------------------")
-# print(-5 // 2)
-# print(5 // 2)
-# print(-5 // 2 + 1)
-# print(5 // 2 + 1)
-# print(-5 // 2 + 1.)
-# print(5 // 2 + 1.)
-# print("-------------------")
-
-# check this by uncommentingg
-
 # --- Gaussian Filter ---
 def gaussian_kernel(size, sigma):
     """Generates a Gaussian kernel."""
@@ -39,20 +27,16 @@ def gaussian_kernel(size, sigma):
 
 gauss_kernel = gaussian_kernel(size=5, sigma=1.5)
 
-print("-------------------")
-print("Gaussian Kernel:\n", gauss_kernel)
-print("Size of Gaussian Kernel:", gauss_kernel.shape)
-print("-------------------")
 
-# --- Apply Convolution Filtering ---
-#gray scale applying----> gaussian working better
-weighted_filtered = convolve2d(gray_original_img, weighted_kernel, mode='same', boundary='fill',fillvalue=0)
-gaussian_filtered = convolve2d(gray_original_img, gauss_kernel, mode='same', boundary='fill',fillvalue=0)
 
 #Color image apply
 b,g,r = cv2.split(orginal_img)
 
-#weighted
+##hsv
+hsv_img = cv2.cvtColor(orginal_img,cv2.COLOR_BGR2HSV)
+h,s,v = cv2.split(hsv_img)
+
+#weighted--bgr
 b_avg = convolve2d(b, weighted_kernel, mode='same', boundary='fill',fillvalue=0)
 g_avg = convolve2d(g, weighted_kernel, mode='same', boundary='fill',fillvalue=0)
 r_avg = convolve2d(r, weighted_kernel, mode='same', boundary='fill',fillvalue=0)
@@ -60,8 +44,16 @@ bgr_avg = cv2.merge((b_avg.astype(np.uint8),
                      g_avg.astype(np.uint8),
                      r_avg.astype(np.uint8)))
 
+#weighted--hsv
+s_avg = convolve2d(s, weighted_kernel, mode='same', boundary='fill',fillvalue=0)
+v_avg = convolve2d(v, weighted_kernel, mode='same', boundary='fill',fillvalue=0)
+hsv_avg = cv2.merge((h.astype(np.uint8),
+                     s_avg.astype(np.uint8),
+                     v_avg.astype(np.uint8)))
+hsv_avg_bgr = cv2.cvtColor(hsv_avg,cv2.COLOR_HSV2BGR)
 
-#gaussian 
+
+#gaussian --bgr
 b_blur = convolve2d(b, gauss_kernel, mode='same', boundary='fill',fillvalue=0)
 g_blur = convolve2d(g, gauss_kernel, mode='same', boundary='fill',fillvalue=0)
 r_blur = convolve2d(r, gauss_kernel, mode='same', boundary='fill',fillvalue=0)
@@ -69,27 +61,29 @@ bgr_blur = cv2.merge((b_blur.astype(np.uint8),
                      g_blur.astype(np.uint8),
                      r_blur.astype(np.uint8)))
 
+#gaussian --hsv
+s_blur = convolve2d(s, gauss_kernel, mode='same', boundary='fill',fillvalue=0)
+v_blur = convolve2d(v, gauss_kernel, mode='same', boundary='fill',fillvalue=0)
+hsv_blur = cv2.merge((h.astype(np.uint8),
+                     s_blur.astype(np.uint8),
+                     v_blur.astype(np.uint8)))
+hsv_blur_bgr = cv2.cvtColor(hsv_blur,cv2.COLOR_HSV2BGR)
 
-
-#median filter 
-median_blur = cv2.medianBlur(median_img,ksize=5)
-
-# #gray difference show
+# #color difference show --bgr,hsv Average
 # cv2.imshow("Original Image",orginal_img)
-# cv2.imshow("Weighted Average Filter", weighted_filtered.astype(np.uint8))
-# cv2.imshow("Gaussian Filter", gaussian_filtered.astype(np.uint8))
-# # cv2.imshow("Median Filter", median_img)
+# cv2.imshow("Weighted Average Filter-BGR", bgr_avg.astype(np.uint8))
+# cv2.imshow("Weighted Average Filter-HSV", hsv_avg_bgr.astype(np.uint8))
 
+#difference calculation 
+diff_bgr_hsv = cv2.absdiff(bgr_blur,hsv_blur_bgr)
+normalize_diff = cv2.normalize(diff_bgr_hsv,None,0,255,cv2.NORM_MINMAX)
+cv2.imshow("Difference btween HSV & BGR",normalize_diff)
 
+#color difference show --bgr,hsv Gaussian
+cv2.imshow("Original Image",orginal_img)
+cv2.imshow("Gaussian Filter-BGR", bgr_blur.astype(np.uint8))
+cv2.imshow("Gaussian Average Filter-HSV", hsv_blur_bgr.astype(np.uint8))
 
-# #color difference show 
-# cv2.imshow("Original Image",orginal_img)
-# cv2.imshow("Weighted Average Filter", bgr_avg.astype(np.uint8))
-# cv2.imshow("Gaussian Filter", bgr_blur.astype(np.uint8))
-
-#median filter show salt and paper 
-cv2.imshow("Original Image",median_img)
-cv2.imshow("Median Filter", median_blur.astype(np.uint8))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
